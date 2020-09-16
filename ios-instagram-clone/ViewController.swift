81714bb424fc2a771cdba3ab96c9c0ba6b190785
Taehyeon-Kim <taehyk528@gmail.com>
2020-09-16 23:47:50 +0900

    Initial Commit

---
+++ ios-instagram-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-instagram-clone
+//
+//  Created by taehy.k on 2020/09/16.
+//  Copyright © 2020 taehy.k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
