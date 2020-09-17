2073ac780a3808692fdcf47be373115b962d2fc7
Taehyeon-Kim <taehyk528@gmail.com>
2020-09-17 11:32:06 +0900

    [ADD] 4개의 swift 파일 추가

---
+++ ios-instagram-clone/Controllers/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  ios-instagram-clone
+//
+//  Created by taehy.k on 2020/09/17.
+//  Copyright © 2020 taehy.k. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ ios-instagram-clone/Controllers/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ios-instagram-clone
+//
+//  Created by taehy.k on 2020/09/17.
+//  Copyright © 2020 taehy.k. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
