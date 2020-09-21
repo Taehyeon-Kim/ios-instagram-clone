dc975522e15d844e9a4637ca56c07f9fb3772635
Taehyeon-Kim <taehyk528@gmail.com>
2020-09-21 14:58:08 +0900

    [ADD] UIView extension file

---
+++ ios-instagram-clone/Resources/Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Extensions.swift
+//  ios-instagram-clone
+//
+//  Created by taehy.k on 2020/09/21.
+//  Copyright © 2020 taehy.k. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
+
+
