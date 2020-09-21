5adc74b0ad8258c163a16a238c47c1d1d89886d5
Taehyeon-Kim <taehyk528@gmail.com>
2020-09-21 15:21:11 +0900

    [ADD] Configure LoginVC Header Area

--- ios-instagram-clone/Controllers/Other/Onboarding/LoginViewController.swift
+++ ios-instagram-clone/Controllers/Other/Onboarding/LoginViewController.swift
@@ -10,18 +10,48 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    struct Constants {
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
+    
     private let usernameEmailField: UITextField = {
-       return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next // 키보드 리턴 타입 -> 주로 .done 사용하는 것 같음
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        return field
     }()
     
     private let passwordField: UITextField = {
-       let field = UITextField()
+        let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password..."
+        field.returnKeyType = .next // 키보드 리턴 타입 -> 주로 .done 사용하는 것 같음
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         return field
     }()
     
     private let loginButton: UIButton = {
-       return UIButton()
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
     }()
     
     private let termsButton: UIButton = {
@@ -33,11 +63,17 @@ class LoginViewController: UIViewController {
     }()
     
     private let createAccountButton: UIButton = {
-       return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("New User? Create an Account", for: .normal)
+        return button
     }()
     
     private let headerView: UIView = {
-       return UIView()
+        let header = UIView()
+        header.backgroundColor = .red
+        header.clipsToBounds = true
+        return header
     }()
     
 
@@ -51,6 +87,14 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         // assign frames
+        // 헤더 뷰 : 로고랑 백그라운드 들어갈 자리
+        headerView.frame = CGRect(
+            x: 0,
+            y: view.safeAreaInsets.top,
+            width: view.width,
+            height: view.height / 3.0
+        )
+        
     }
     
     private func addSubviews(){
